f76cf501c5969e7b6d81cbfe5057a322606b94da
Oded Lazar <odedlaz@gmail.com>
2017-07-27 00:32:55 +0300

    Minor server refactoring

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
-const app = require('express')()
-const request = require('request')
-const bodyParser = require('body-parser')
-const upload = require('multer')()
+const app = require('express')();
+const request = require('request');
+const bodyParser = require('body-parser');
+const upload = require('multer')();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.header('Content-Type', 'application/json')
-  next()
-})
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+             'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Content-Type', 'application/json');
+  next();
+});
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+function req_params(req) {
+  return {
+    url : 'https://github.com/login/oauth/access_token',
+    form : req.body,
+    headers : {
+      'Accept' : 'application/json',
+      'User-Agent' : 'gh-oauth-server',
+    }
+  };
+};
 
 app.post('*', upload.array(), (req, res) => {
-  request.post({
-    url: 'https://github.com/login/oauth/access_token',
-    form: req.body,
-    headers: {
-      'Accept': 'application/json',
-      'User-Agent': 'gh-oauth-server',
-    },
-  }, (error, r, body) => {
+  request.post(req_params(req), (error, r, body) => {
     if (!error) {
-      res.send(body)
+      res.send(body);
     } else {
-      res.json({ error })
+      res.json({error});
     }
-  })
-})
+  });
+});
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`gh-oauth-server listening on port ${port}`))
+const port = process.env.PORT || 3000;
+app.listen(port,
+           () => console.log(`gh-oauth-server listening on port ${port}`));
